c20cea0ce603e4de0d83526845f2d5ad0b163b2e
Umniyah <umniyah.mohammed@gmail.com>
2021-09-17 13:20:56 -0400

    message about changes goes here

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyInfoCollection = new ArrayList<BuddyInfo>();
+
+    public void addBuddy(BuddyInfo b){
+        buddyInfoCollection.add(b);
+    }
+
+    public void removeBuddy(BuddyInfo b){
+        buddyInfoCollection.remove(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+
+        BuddyInfo buddy = new BuddyInfo("Umniyah");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+
+    }
+
+}
